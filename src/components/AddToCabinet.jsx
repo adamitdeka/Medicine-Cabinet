36bc66572442d3c5018daf7f7a3c26ad31893b62
Amit Deka <adamitdeka@gmail.com>
2022-04-14 21:25:20 -0400

    first commit

--- src/components/AddToCabinet.jsx
+++ src/components/AddToCabinet.jsx
@@ -1,10 +1,10 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { Button } from "react-bootstrap";
 import { addDrug } from "../store/Actions";
 import { Store } from "../store/Store";
 
 function AddToCabinet(props) {
-  const { state, dispatch } = useContext(Store);
+  const { dispatch } = useContext(Store);
   const handleAdd = () => {
     addDrug(props.data, dispatch);
   };
